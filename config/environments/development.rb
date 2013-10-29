9cabb128284dbef8f3eb3a7d4d58d76ba4600f47
Steven Abrams <sabrams@arwilfley.com>
2013-10-29 10:39:17 -0600

    added bootstrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,13 @@ Engdb::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.bizmail.yahoo.com",
+    port: 110,
+    domain: "wilfley.com",
+    authentication: "plain",
+    user_name: "sabrams@arwilfley.com",
+    password: "sawilfley",
 end
