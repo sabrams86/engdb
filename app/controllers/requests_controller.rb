750f566c2ff340c4b5bce40c1dbfd71446644ad5
Steve <steven.abrams86@gmail.com>
2013-11-21 00:02:55 -0500

    added pagination to SIRs, working on Request emails

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,10 +1,13 @@
 class RequestsController < ApplicationController
+  handles_sortable_columns
   before_filter :sales_check, only: [:create, :new]
   before_filter :admin_check, only: [:destroy]
   # GET /requests
   # GET /requests.json
   def index
-    @requests = Request.all
+    order = sortable_column_order, "request_number desc"
+    @requests = Request.paginate page: params[:page], order: order, per_page: 100
+    
 
     respond_to do |format|
       format.html # index.html.erb
@@ -83,4 +86,38 @@ class RequestsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+  def submit_eng
+      
+    @request = Request.find(params[:id])
+    
+    
+    respond_to do |format|
+      RequestNotifier.submit_engineering(@request).deliver if @ecn.distribute_engineering?
+
+      format.html { redirect_to ecns_url, alert: "Ecn has been submitted for approval." }
+      format.json { render json: @ecns }
+    end
+  end
+  
+  def create_sor
+    @ecn = Ecn.find(params[:id])
+    @ecn = @ecn.close_status(@ecn)
+    
+    respond_to do |format|
+      EcnNotifier.close_engineering(@ecn).deliver if @ecn.distribute_engineering?
+      EcnNotifier.close_purchasing(@ecn).deliver if @ecn.distribute_purchasing?
+      EcnNotifier.close_manufacturing(@ecn).deliver if @ecn.distribute_manufacturing?
+      EcnNotifier.close_qantel(@ecn).deliver if @ecn.distribute_qantel?
+      EcnNotifier.close_planning(@ecn).deliver if @ecn.distribute_planning?
+      EcnNotifier.close_sales(@ecn).deliver if @ecn.distribute_sales?
+      EcnNotifier.close_inventory(@ecn).deliver if @ecn.distribute_inventory?
+      EcnNotifier.close_quality(@ecn).deliver if @ecn.distribute_quality?
+      EcnNotifier.close_india(@ecn).deliver if @ecn.distribute_india?
+      EcnNotifier.close_finance(@ecn).deliver if @ecn.distribute_finance?
+      format.html { redirect_to ecns_url, alert: "Ecn has been closed.  A confirmation email has been sent to the appropriate personnel." }
+      format.json { render json: @ecns }
+    end
+  end
+  
 end
