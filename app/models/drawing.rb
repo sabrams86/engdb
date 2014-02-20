9bc29b3c281254a37cd0826712a875cac6b19332
Steven Abrams <sabrams@wilfley.com>
2014-02-20 08:23:15 -0700

    added gitignore

--- app/models/drawing.rb
+++ app/models/drawing.rb
@@ -59,7 +59,7 @@ class Drawing < ActiveRecord::Base
   def draw_path(drawing)
     folder = drawing[:file_location]
     file_name = drawing[:drawing_number]
-      path = Dir["smb://engserver/engineering/shop\ prints/#{folder}/#{file_name}*"].first
+      path = Dir["SMB://engserver/engineering/shop\ prints/#{folder}/#{file_name}*"].first
     return path
   end
 
