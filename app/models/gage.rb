710fffcc12eb3e65de7ed130d0cca7b101530cc1
Jesse Macku <Jmacku@wilfley.com>
2015-06-16 10:45:01 -0600

    Gage changes for Scott round 3

--- app/models/gage.rb
+++ app/models/gage.rb
@@ -1,7 +1,7 @@
 class Gage < ActiveRecord::Base
-  attr_accessible :calibration_date, :calibration_period, :description, :gage_number, :gage_type, :location, :rating, :servicing_company, :servicing_phone, :status
+  attr_accessible :calibration_date, :calibration_period, :description, :gage_number, :gage_type, :location, :manufacturer, :servicing_company, :servicing_phone, :status
 
-  validates :gage_number, :gage_type, :description, :rating, :calibration_date, :calibration_period, :status, presence: true
+  validates :gage_number, :gage_type, :description, :rating, :calibration_last_done, :calibration_due, :calibration_period, :status, presence: true
   validates :gage_number, uniqueness: true
 
   PERIOD = [ "Yearly", "Every 3 Years", "Monthly" ]
